4b981be9d11e0e9f7d769bb7ca7566551c3c2478
David Lebsack <dlebsack@conquer.io>
2025-03-09 09:54:17 -0500

    update model on chatgpt nvim

--- nvim/lua/custom/plugins/chatGPT.lua
+++ nvim/lua/custom/plugins/chatGPT.lua
@@ -3,6 +3,9 @@ return {
 	event = "VeryLazy",
 	config = function()
 		require("chatgpt").setup({
+			openai_params = {
+				model = "gpt-4-1106-preview",
+			},
 			edit_with_instructions = {
 				keymaps = {
 					accept = "<C-a>",
